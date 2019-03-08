f8f464b65c7d5eba8dcd25b69f3089e77947a328
Stakh Jurii <stakh.jurii@gmail.com>
2019-03-08 12:15:46 +0200

    Added our first hello program

---
+++ hello-git-lesson/HelloAll.java
@@ -0,0 +1,17 @@
+public class HelloAll {
+
+public static void main (String [] args) {
+
+System.out.println("Hello all !!!")
+//================================
+printUserInfo("Stakh", "Jurii");
+
+}
+
+public static void printUserInfo (String firstname , String lastName) {
+
+System.out.format("Hello %s %s, Welcome to Hillel school, \n ", firstName, lastName);
+
+}
+
+}
